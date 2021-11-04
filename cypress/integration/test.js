636bf7b7c4d56f620b8f94e302a9e22fdaa9f8b8
falad001 <adrian.falenczyk@staples-solutions.com>
2021-11-04 13:46:55 +0100

    finalized adding to cart

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,13 +1,33 @@
+//header component
 var cookieAccept = '[onclick="Cookiebot.submitCustomConsent(true, true, true); hideCookieBanner();"]';
 var signInButton = "a[class*='login'] span[class='hidden-xs']";
+//auth component
+var userNameField = '#ShopLoginForm_Login';
+var passwordField = '#ShopLoginForm_Password';
+var submitButton = '.col-sm-9 > .btn-primary';
+//plp component
+var addToCartButtons = 'button[class="btn spls-add-to-cart-btn btn-lg btn-block btn-primary stock-check"]';
+//minicart component
+var miniCartButton = '.quick-cart-link';
+var viewCartButton = '.view-cart';
+//cart component
+var cartItem = ':nth-child(3) > .list-item-row';
+var cartItemTitle = '.text-dark-bold';
+var cartItemSku = '.product-id';
+var cartItemPrice = '.list-item-row > .column-price';
 
-describe("tezt", function() {
-    it("Logins to storefront", navigateToStorefront);
-    it("Closes the cookies", closeCookies);
+describe("Add a product to cart", function () {
+    it("Navigates to storefront", navigateToStorefront);
+    it("Accepts the cookies", closeCookies);
     it("Signs in", signIn);
+    it("Search for all products", searchForAll);
+    it("Adds any product to cart", addRandomProductToCart);
+    it("Goes to cart", navigateToCart);
+    it("Checks the Cart", verifyPDPElements);
 })
 
 function navigateToStorefront() {
+    cy.clearCookies();
     cy.visit("https://www-uat-live.pressel.at/");
 }
 
@@ -17,4 +37,35 @@ function closeCookies() {
 
 function signIn() {
     cy.get(signInButton).click();
+    cy.get(userNameField).type("adrian.falenczyk@staples-solutions.com");
+    cy.get(passwordField).type("Testowe!2345");
+    cy.get(submitButton).click();
+}
+
+function searchForAll() {
+    cy.get('#spls-header-search-bar-id').type("*{enter}");
+}
+
+function addRandomProductToCart() {
+    cy.get(addToCartButtons, { timeout: 10000 })
+        .should('be.enabled')
+        .eq(Math.floor(Math.random() * 3))
+        .click({ force: true, multiple: true });
+    cy.get('.loader-wrapper', { timeout: 10000 }).should('be.visible');
+}
+
+function navigateToCart() {
+    cy.get(miniCartButton).click();
+    cy.get(viewCartButton).click();
+}
+
+function verifyPDPElements() {
+    checkIfElementIsVisible(cartItem);
+    checkIfElementIsVisible(cartItemTitle);
+    checkIfElementIsVisible(cartItemPrice);
+    checkIfElementIsVisible(cartItemSku);
+}
+
+function checkIfElementIsVisible(String) {
+    cy.get(String, { timeout: 10000 }).should('be.visible');
 }
